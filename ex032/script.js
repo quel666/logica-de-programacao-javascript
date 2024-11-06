dc24426a62dab85da107054ef7db5ac87ae94ec6
Raquel <168922095+quel666@users.noreply.github.com>
2024-11-05 21:19:58 -0300

    Add files via upload

---
+++ ex032/script.js
@@ -0,0 +1,11 @@
+var peso = Number(prompt("Digite a seu peso:"))
+var altura = Number(prompt("Digite a sua altura:"))
+
+var imc = (peso / altura) ** altura 
+
+if (imc < 18.5) {
+    alert ("Abaixo do peso")
+}
+
+else if (imc)
+
